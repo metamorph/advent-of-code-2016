a56f15766fa78669ee5387486ce8b7c12d5db6f4
Anders Engström <anders.engstrom@leogears.com>
2016-12-05 00:44:57 +0100

    wip

--- security/src/security/core.clj
+++ security/src/security/core.clj
@@ -4,12 +4,21 @@
   [s]
   (let [[m a b c] (re-find #"([a-z-]+)-([0-9]+)\[([a-z]+)\]" s)]
     (if m
-      {:name a :sector-id b :checksum c}
+      {:name a :sector-id (Integer/parseInt b) :checksum c}
       (throw (IllegalArgumentException. (str "Not a valid room: " s))))))
 
-(defn valid-room? [{:keys [name sector-id checksum]}] false)
+(defn name->checksum "TODO: Doesn't work - only sort alphabetical if a tie!"
+  [name]
+  (let [chars (filter #(not (= \- %)) name)
+        by-char (frequencies chars)
+        most-frequent-chars (map first (reverse (sort-by last by-char)))]
+    (apply str (take 5 (sort most-frequent-chars)))))
+
+
+(defn valid-room? [{:keys [name checksum]}] (= checksum (name->checksum name)))
+
 
 (defn sum-sector-ids [input]
   (let [rooms (map enc->room input)
         valid (filter valid-room? rooms)]
-    (reduce + 0 (map :sector-id))))
+    (reduce + 0 (map :sector-id valid))))
