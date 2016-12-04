78f08696e4c1947c3405afe7ee7ef5b275c4e0fd
Anders Engström <anders.engstrom@leogears.com>
2016-12-05 00:23:55 +0100

    Implement enc->room

--- security/src/security/core.clj
+++ security/src/security/core.clj
@@ -1,7 +1,11 @@
 (ns security.core)
 
-(defn enc->room [s]
-  s)
+(defn enc->room "Parse a string to a room struct (name/sector-id/checksum)."
+  [s]
+  (let [[m a b c] (re-find #"([a-z-]+)-([0-9]+)\[([a-z]+)\]" s)]
+    (if m
+      {:name a :sector-id b :checksum c}
+      (throw (IllegalArgumentException. (str "Not a valid room: " s))))))
 
 (defn valid-room? [{:keys [name sector-id checksum]}] false)
 
