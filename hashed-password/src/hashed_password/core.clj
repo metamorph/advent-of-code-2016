6502f2c37e0ed254e3b2dcda89b2fec1274ea793
Anders Engström <anders.engstrom@leogears.com>
2016-12-06 08:30:39 +0100

    Hacking - speed it up..

--- hashed-password/src/hashed_password/core.clj
+++ hashed-password/src/hashed_password/core.clj
@@ -1,9 +1,9 @@
-(ns hashed-password.core)
+(ns hashed-password.core
+  (:gen-class)
+  (:require digest))
 
-(defn valid-hash? [s]
-  (clojure.string/starts-with? s "00000"))
-(defn hash->digit [s]
-  (first (drop 5 s)))
+(defn valid-hash? [s] (clojure.string/starts-with? s "00000"))
+(defn hash->digit [s] (first (drop 5 s)))
 (defn hash-generator [room-id]
   (let [initial {:hash ""
                  :idx 1}]
@@ -20,3 +20,14 @@
                         (map #(hash->digit (:hash %)))
                         (take 8))
                        (hash-generator room-id))))
+
+(def find-hashes-tail [room-id]
+  (let [hashes (hash-generator room-id)]
+    (loop [hashes hashes
+           result []]
+
+      (recur ))))
+
+(defn -main [& args]
+  (printf "Password is: %s\n" (hack-password "uqwqemis")))
+
