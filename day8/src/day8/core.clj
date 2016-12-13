1cc3581a13109c13c104575b8bcebcaa73ca6e40
Anders Engström <anders.engstrom@leogears.com>
2016-12-13 13:54:39 +0100

    Make condp look nicer

--- day8/src/day8/core.clj
+++ day8/src/day8/core.clj
@@ -8,24 +8,20 @@
        (repeat height)
        (vec)))
 
-(defn rotate [coll n] (vec (take (count coll)
-                             (drop (- (count coll) n)
-                                   (cycle (seq coll))))))
-(defn instruction->fn [s]
-  (condp re-find s
-    #"rect (\d+)x(\d+)" :>> #(let [[_ x y] %]
-                               (fn [d] (rect d (Integer/parseInt x) (Integer/parseInt y))))
-    #"rotate column x=(\d+) by (\d+)" :>> #(let [[_ col n] %] (fn [d] (rotate-column d (Integer/parseInt col) (Integer/parseInt n))))
-    #"rotate row y=(\d+) by (\d+)" :>> #(let [[_ row n] %] (fn [d] (rotate-row d (Integer/parseInt row) (Integer/parseInt n))))
-    (throw (IllegalArgumentException. (str "Illegal input: " s)))))
 
 (defn update-column [display n f]
-  (let [vals (map #(get % n) display)
+  (let [vals    (map #(get % n) display)
         updated (f vals)]
     (mapv #(assoc %1 n %2) display updated)))
 
 (defn plot-at [display x y v]
   (update display y #(assoc % x v)))
+
+(defn s->int [s] (Integer/parseInt s))
+
+(defn rotate [coll n] (vec (take (count coll)
+                                 (drop (- (count coll) n)
+                                       (cycle (seq coll))))))
 
 (defn rotate-column [display col n] (update-column display col #(rotate % n)))
 (defn rotate-row [display row n] (update display row rotate n))
@@ -34,6 +30,15 @@
                      y (range height)] [x y])]
     (reduce (fn [d [x y]] (plot-at d x y \#)) display coords)))
 
+(defn unary-rect-fn [x y] #(rect % x y))
+(defn unary-rotate-column-fn [col n] #(rotate-column % col n))
+(defn unary-rotate-row-fn [row n] #(rotate-row % row n))
+(defn instruction->fn [s]
+  (condp re-find s
+    #"rect (\d+)x(\d+)"               :>> (fn [[_ x y]] (unary-rect-fn (s->int x) (s->int y)))
+    #"rotate column x=(\d+) by (\d+)" :>> (fn [[_ col n]] (unary-rotate-column-fn (s->int col) (s->int n)))
+    #"rotate row y=(\d+) by (\d+)"    :>> (fn [[_ row n]] (unary-rotate-row-fn (s->int row) (s->int n)))
+    (throw (IllegalArgumentException. (str "Illegal input: " s)))))
 (defn transform [display instructions]
   (let [fns (map instruction->fn instructions)]
     (reduce #(%2 %1) display fns)))
